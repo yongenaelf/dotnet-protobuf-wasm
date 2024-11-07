77d363a83e7f26d0a9aa17e29bec170809c41069
yongen.loong <>
2024-11-07 20:57:40 +0800

    fix: build script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
+./dotnet/dotnet publish Client -c Release -o output
